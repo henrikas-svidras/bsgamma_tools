e9595d94fcbfaa76a5b8979c5422432a9e42ac7e
Henrikas Svidras <henrikas.svidras@desy.de>
2022-12-15 18:37:27 +0100

    lighten or darken a color function

--- bsgamma_tools/helpers.py
+++ bsgamma_tools/helpers.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import string
 import warnings
+import matplotlib.colors as mc
+import colorsys
 
 def pdg_to_name(input_id, latex=False, latex_unready=False):
     """
@@ -263,3 +265,19 @@ def create_bins(input_list, var = 'gamma_EB'):
         beanlist.append(f'{b}>{var}>={a}')
     return beanlist
 
+def lighten_color(color, amount=0.5):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
